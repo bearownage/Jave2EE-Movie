8ed328e51ccbdfc60ea7db8b8b6e50ec969b3299
Husain Adam Askari <haskari@ucsc.edu>
2020-09-02 15:29:00 -0700

    some none functional code

---
+++ src/main/test/usermanagement/web/CartServletTest.java
@@ -0,0 +1,42 @@
+package usermanagement.web;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import usermanagement.web.CartServlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class CartServletTest {
+	@Mock private HttpServletRequest request;
+    @Mock private HttpServletResponse response;
+    @Mock private RequestDispatcher requestDispatcher;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+    
+    @Test
+    public void doGet() throws Exception {
+    	StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+        
+        System.out.println(request + " " + response);
+        new CartServlet().doGet(request, response);
+        
+        assertEquals("Random", stringWriter.toString());
+    }
+    
+    
+}
